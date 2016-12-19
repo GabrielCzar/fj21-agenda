f1a2a39f1636d40aa42e19796c04ff4e3d4a9af3
Gabriel Cesar <gcal1@live.com>
2016-12-18 23:52:40 -0300

    funcionando normalmente

--- src/br/com/caelum/jdbc/ConnectionFactory.java
+++ src/br/com/caelum/jdbc/ConnectionFactory.java
@@ -7,9 +7,10 @@ import java.sql.SQLException;
 public class ConnectionFactory {
     public Connection getConnection() {
         try {
+        	Class.forName("com.mysql.jdbc.Driver");
             return DriverManager.getConnection(
           "jdbc:mysql://localhost:3306/agenda", "root", "");
-        } catch (SQLException e) {
+        } catch (SQLException | ClassNotFoundException e) {
             throw new RuntimeException(e);
         }
     }
